df18c49866f42c663c5399fd30ab19469efc760c
erickterri <erick-terri-10@hotmail.com>
2015-10-05 22:43:59 -0500

    model y Dao

---
+++ ProyectoBDDM/proyectoBDDM/src/java/dao/UsuarioDao.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import model.Usuario;
+
+/**
+ *
+ * @author 
+ */
+public class UsuarioDao {
+    
+     public static List<Usuario> buscarUsuarios() {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        CallableStatement cs = null;
+        ResultSet rs = null;
+        try {
+            List<Usuario> empleados = new ArrayList();
+            cs = connection.prepareCall("{ call listaUsuarios() }");
+            rs = cs.executeQuery();
+            while (rs.next()) {
+                Usuario emp = new Usuario(
+                        rs.getInt("id"), 
+                        rs.getString("nombre"), 
+                        rs.getString("apellido"), 
+                        rs.getString("username"),
+                        rs.getString("password"),
+                        rs.getString("puesto")
+                );
+
+                empleados.add(emp);
+            }
+            return empleados;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+            
+        } finally {
+            DBUtil.closeResultSet(rs);
+            DBUtil.closeStatement(cs);
+            pool.freeConnection(connection);
+        }
+    }
+    
+     public static void insertarUsuario(Usuario e) {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        CallableStatement cs = null;
+        try {
+            cs = connection.prepareCall("{ call insertUsuario(?, ?, ?, ?) }");
+            cs.setString(1, e.getNombre());
+            cs.setString(2, e.getApellido());
+            cs.setString(3, e.getUsername());
+            cs.setString(4, e.getPassword());
+            cs.setString(5, e.getPuesto());
+            cs.execute();
+            
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            
+        } finally {
+            DBUtil.closeStatement(cs);
+            pool.freeConnection(connection);
+        }
+    }
+}
---
+++ ProyectoBDDM/proyectoBDDM/src/java/model/Usuario.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author 
+ */
+public class Usuario {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String username;
+    private String password;
+    private String puesto;
+
+    public Usuario(int id, String nombre, String apellido, String username, String password, String puesto) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.username = username;
+        this.password = password;
+        this.puesto = puesto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+    
+    
+}
